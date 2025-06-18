e3202b44ede293c8d4d730c1eaa60d75f4172182
Princy chahal <princychahal68@gmail.com>
2025-06-17 18:41:37 -0700

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @returns {number} the length of the array
  */
 function getLength(numbers) {
-  // TODO
+  return numbers.length
 }
 
 /**
@@ -11,7 +11,13 @@ function getLength(numbers) {
  * @returns {number} the sum of the numbers
  */
 function getSum(numbers) {
-  // TODO
+  let res;
+  res = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    const element = array[i];
+    res = res+element;  
+  }
+  return res;
 }
 
 /**
@@ -19,7 +25,8 @@ function getSum(numbers) {
  * @returns {number} the mean of the numbers
  */
 function getMean(numbers) {
-  // TODO
+  let res = getSum(numbers)/getLength(numbers);
+  return res;
 }
 
 /**
@@ -27,7 +34,13 @@ function getMean(numbers) {
  * @returns {number} the smallest of the numbers
  */
 function getMin(numbers) {
-  // TODO
+  let min = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < min){
+      min = numbers[i];
+    }  
+  }
+  return min;
 }
 
 /**
@@ -35,7 +48,13 @@ function getMin(numbers) {
  * @returns {number} the largest of the numbers
  */
 function getMax(numbers) {
-  // TODO
+  let max = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }  
+  }
+  return max;
 }
 
 /**
@@ -43,7 +62,7 @@ function getMax(numbers) {
  * @returns {number} the range of the numbers (max - min)
  */
 function getRange(numbers) {
-  // TODO
+  return getMax(numbers) - getMin(numbers)
 }
 
 /**
@@ -51,7 +70,13 @@ function getRange(numbers) {
  * @returns {number[]} the even numbers in the array
  */
 function getEvens(numbers) {
-  // TODO
+  let output = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i]%2 === 0) {
+      output.push(numbers[i]);
+    }  
+  }
+  return output;
 }
 
 /**
@@ -59,7 +84,13 @@ function getEvens(numbers) {
  * @returns {number[]} the odd numbers in the array
  */
 function getOdds(numbers) {
-  // TODO
+  let output;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i]%2 !== 0){
+      output.push(numbers[i]);
+    }  
+  }
+  return output
 }
 
 // === READ BUT DO NOT EDIT THE CODE BELOW ===
